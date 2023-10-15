1d0b67f03c3b995967f2044b63e86b57ab1d1a59
PoisonNF <903131263@qq.com>
2023-10-15 16:02:02 +0800

    增加SGA串口DMA发送例程

---
+++ SGA串口DMA发送例程/Apply/Logic/Src/usercode.c
@@ -0,0 +1,38 @@
+#include "usercode.h"		/* usercode头文件 */
+#include "drv_hal_conf.h"   /* SGA库头文件配置 */
+#include "task_conf.h"      /* task层头文件配置 */
+#include "ocd_conf.h"       /* OCD层头文件配置 */
+#include "dev_conf.h"		/* Dev层头文件配置 */
+#include "algo_conf.h"		/* Algo层头文件配置 */
+#include "config.h"			/* I/O配置头文件配置 */
+
+/**
+ * 该例程实现从串口1DMA接收数据，并从串口1使用DMA发送的方式返还数据。
+ * 
+ */
+
+/* 用户逻辑代码 */
+void UserLogic_Code(void)
+{
+	uint8_t num;					//记录存储字节数
+	uint8_t ReceBuffer[100];		//接收数据buffer
+	uint8_t SendBuffer[200];		//发送数据buffer
+
+	printf("SGA_DEMO\r\n");
+
+	while(1)
+	{
+		//从串口1DMA接收数据，存放到buffer中
+		num = Drv_Uart_Receive_DMA(&demoUart,ReceBuffer);
+
+        //接收到的数据个数不为0
+		if(num != 0)
+		{
+			printf("num = %d\r\n",num);
+			memcpy(SendBuffer,ReceBuffer,num);						//内存拷贝
+            Drv_Uart_Transmit_DMA(&demoUart,SendBuffer,num);		//DMA发送
+        
+			num = 0;
+		}
+	}
+}
---
+++ SGA串口DMA发送例程/Apply/Task/Src/task_userinit.c
@@ -0,0 +1,9 @@
+#include "task_conf.h"
+#include "ocd_conf.h"
+#include "config.h"
+
+/* 外设初始化函数 */
+void Task_UserInit(void)
+{
+    Drv_Uart_DMAInit(&demoUart);
+}
